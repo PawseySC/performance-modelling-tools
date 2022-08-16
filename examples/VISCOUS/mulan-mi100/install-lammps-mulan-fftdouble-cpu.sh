ea541a479d59f208abf0a543012f64f16640f9b2
Marco De La Pierre <marco.delapierre@gmail.com>
2022-08-16 14:29:04 +0800

    viscous lammps: adding install scripts for CPU-only Kokkos

---
+++ examples/VISCOUS/mulan-mi100/install-lammps-mulan-fftdouble-cpu.sh
@@ -0,0 +1,60 @@
+#!/bin/bash -l
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --threads-per-core=1
+#SBATCH --time=01:00:00
+#SBATCH --output=out-install-lammps-mulan-fftdouble-cpu
+
+# Lammps with Kokkos for OpenMP
+# FFTs: double precision, host KISS
+
+reponame="lammps"
+repo="git@github.com:CTCMS-UQ/${reponame}.git"
+checkout="sprint-develop"
+dir="lammps-mulan-fftdouble-cpu"
+group="sprint4"
+umask 007
+
+echo BUILD-START $( date )
+
+module unload gcc/9.3.0
+
+rm -fr $dir
+mkdir -p $dir
+cd $dir
+
+git clone $repo
+cd $reponame
+git checkout $checkout
+
+cd src
+sed -e 's/KOKKOS_DEVICES *=.*/KOKKOS_DEVICES = OpenMP,Serial\
+KOKKOS_ARCH = ZEN2/' \
+    -e 's/CC *=.*/CC = CC/g' \
+    -e 's/LINK *=.*/LINK = CC/g' \
+	-e 's/FFT_INC *=.*/FFT_INC = /g' \
+    -e '/MPI_INC *=/ s;$; -I${MPICH_DIR}/include;g' \
+    -e '/MPI_LIB *=/ s;$; -L${MPICH_DIR}/lib -lmpi -L${CRAY_MPICH_ROOTDIR}/gtl/lib -lmpi_gtl_hsa;g' \
+    MAKE/OPTIONS/Makefile.kokkos_mpi_only >MAKE/Makefile.mulan_cpu
+
+# issue with finding libatomic at link stage
+sed -i '/KOKKOS_LIBS *+= *-latomic/ s/^/#/g' ../lib/kokkos/Makefile.kokkos
+
+#make no-all
+make yes-CLASS2
+make yes-MANYBODY
+make yes-MISC
+make yes-EXTRA-COMPUTE
+make yes-EXTRA-DUMP
+make yes-EXTRA-FIX
+make yes-KSPACE
+make yes-MOLECULE
+make yes-RIGID
+make yes-MOLFILE
+make yes-UEF
+make yes-MOL-SLLOD
+make yes-KOKKOS
+
+sg $group -c 'make -j 8 mulan_cpu'
+
+echo BUILD-END $( date )
---
+++ examples/VISCOUS/topaz-v100/install-lammps-topaz-fftsingle-cpu.sh
@@ -0,0 +1,71 @@
+#!/bin/bash -l
+#SBATCH --partition=gpuq-dev
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=01:00:00
+#SBATCH --output=out-install-lammps-topaz-fftsingle-cpu
+
+# Lammps with Kokkos for OpenMP
+# FFTs: single precision, host FFTW
+
+reponame="lammps"
+repo="git@github.com:CTCMS-UQ/${reponame}.git"
+checkout="sprint-develop"
+dir="lammps-topaz-fftsingle-cpu"
+group="sprint4"
+umask 007
+
+echo BUILD-START $( date )
+
+module swap gcc gcc/10.2.0
+module load cuda/11.4.2
+module load openmpi-ucx-gpu/4.0.2
+module load fftw/3.3.8
+
+module load cmake/3.18.0
+
+rm -fr $dir
+mkdir -p $dir
+cd $dir
+mkdir -p app
+install_dir="$(pwd)/app"
+
+git clone $repo
+cd $reponame
+git checkout $checkout
+
+mkdir build
+cd build
+
+cmake ../cmake \
+ -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
+ -DCMAKE_INSTALL_PREFIX="$install_dir" \
+ -DCMAKE_CXX_COMPILER="mpicxx" \
+ -DCMAKE_C_COMPILER="mpicc" \
+ -DCMAKE_Fortran_COMPILER="mpif90" \
+ -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-g -O3 -DNDEBUG" \
+ -DPKG_CLASS2=ON \
+ -DPKG_MANYBODY=ON \
+ -DPKG_MISC=ON \
+ -DPKG_EXTRA-COMPUTE=ON \
+ -DPKG_EXTRA-DUMP=ON \
+ -DPKG_EXTRA-FIX=ON \
+ -DPKG_KSPACE=ON \
+ -DPKG_MOLECULE=ON \
+ -DPKG_RIGID=ON \
+ -DPKG_MOLFILE=ON \
+ -DPKG_UEF=ON \
+ -DPKG_MOL-SLLOD=ON \
+ -DPKG_KOKKOS=ON \
+ -DFFT=FFTW3 \
+ -DFFT_SINGLE=True \
+ -DKokkos_ARCH_SKX=ON \
+ -DKokkos_ENABLE_SERIAL=ON \
+ -DKokkos_ENABLE_OPENMP=ON \
+ -DBUILD_OMP=ON \
+ -DBUILD_MPI=ON
+
+sg $group -c 'make -j 8'
+sg $group -c 'make install'
+
+echo BUILD-END $( date )
