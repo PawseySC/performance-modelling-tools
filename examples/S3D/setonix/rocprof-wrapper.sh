14b5226ae91138949989bea6e77beab42d6e0e5d
Joe Schoonover <joe@fluidnumerics.com>
2023-07-25 01:29:15 +0800

    Fix multi-gpu profiling wrapper setup for s3d

--- examples/S3D/setonix/rocprof-wrapper.sh
+++ examples/S3D/setonix/rocprof-wrapper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 set -x 
-cp rocprof-template.txt profile-$SLURM_PROCID.txt
-rocprof -i profile-$SLURM_PROCID.txt --stats --sys-trace ./demo > log-$SLURM_PROCID.txt
+cp rocprof-template.txt profile-${SLURM_PROCID}.txt
+rocprof -i profile-${SLURM_PROCID}.txt ./s3d.STXgpu.x > log-${SLURM_PROCID}.txt
+rocprof -o profile-${SLURM_PROCID}.csv --stats --sys-trace ./s3d.STXgpu.x
--- examples/S3D/setonix/run_profile.s3d_STXgpu.sh
+++ examples/S3D/setonix/run_profile.s3d_STXgpu.sh
@@ -24,13 +24,7 @@ cwd=$(pwd)
 timestamp=$(date +"%Y-%m-%d-%H-%M")
 odir="${cwd}/profile_${CHECKOUT_DIR}_${INPUT_DECK}_${timestamp}"
 
-
 module load PrgEnv-cray rocm/${ROCM_VERSION} craype-accel-amd-gfx90a 
-#module load gcc/11.2.0 omnitrace/1.10.0
-#export PATH=$PATH:/software/setonix/2022.11/custom/software/zen3/gcc/12.1.0/omnitrace/1.10.0/bin
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/setonix/2022.11/custom/software/zen3/gcc/12.1.0/omnitrace/1.10.0/lib/:/software/setonix/2022.11/pawsey/software/rocm/rocm-5.4.3rev1/lib
-#OMNITRACE="/software/setonix/2022.11/custom/software/zen3/gcc/12.1.0/omnitrace/1.10.0/bin/omnitrace"
-
 
 export OMP_NUM_THREADS=1
 export MPICH_GPU_SUPPORT_ENABLED=1
@@ -38,24 +32,18 @@ export MACH
 
 # Copy the input deck to input/s3d.in
 cp ${cwd}/${INPUT_DECK} ${cwd}/${CHECKOUT_DIR}/input/s3d.in
+cp ${cwd}/rocprof-template.txt ${cwd}/${CHECKOUT_DIR}/run
 
 cd ${cwd}/${CHECKOUT_DIR}/run
-#omnitrace-instrument -o ./s3d.STXgpu.x.inst -- ./s3d.STXgpu.x
 
 # Straight run of the code
 #srun -u -n $SLURM_NTASKS -c 8 ./s3d.STXgpu.x
 
 # Run for hotspot profile
-#srun -n $SLURM_NTASKS -c 8 rocprof --stats ./s3d.STXgpu.x
-srun -n $SLURM_NTASKS -c 8 rocprof --sys-trace ./s3d.STXgpu.x
+srun -n $SLURM_NTASKS -c 8 ${cwd}/rocprof-wrapper.sh
 
-#mkdir -p ${odir}
-#mv results.*.csv ${odir}/
-
-# Run for trace profile
-#srun -u -n $SLURM_NTASKS -c 8 rocprof --sys-trace ./s3d.STXgpu.x
-#mv results.json ${odir}/
-
-# Run for events profile
-#srun -u -n $SLURM_NTASKS -c 8 rocprof -i rocprof_counters.txt -o ${odir}/rocprof_metrics.csv ./s3d.STXgpu.x
-
+mkdir -p ${odir}
+mv profile*.csv ${odir}/
+mv profile*.json ${odir}/
+mv profile*.db ${odir}/
+mv profile*.txt ${odir}/
